0d2caa75ea55e4324b11f66e1b0d613691a9ae6a
argvsc47 <55107887+argvsc47@users.noreply.github.com>
2021-07-07 22:34:28 +0100

    Update monkepad.py

--- monkepad.py
+++ monkepad.py
@@ -91,7 +91,7 @@ def save_file(event):
     FILE_CONTENT = text_to_save
     file.close()
 
-    print(f'{FILE_NAME} saved successfully')
+    print(f'{file.name} saved successfully')
 
 menubar = tk.Menu(root)
 filemenu = tk.Menu(menubar, tearoff=0)
